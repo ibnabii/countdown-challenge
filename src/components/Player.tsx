625489fe2db281b130a3edd0fb07c2ce630ccf31
ibnabii <someone@somewhere.net>
2024-08-16 19:42:14 +0200

    with ref

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,19 +1,17 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
   const [playerName, setPlayerName] = useState<string>("unknown entity");
-  const [enteredName, setEnteredName] = useState<string>("");
+  const enteredName = useRef<HTMLInputElement>(null);
 
   return (
     <section id="player">
       <h2>Welcome {playerName}</h2>
       <p>
-        <input
-          type="text"
-          id="playerNameInput"
-          onChange={(e) => setEnteredName(e.target.value)}
-        />
-        <button onClick={() => setPlayerName(enteredName)}>Set Name</button>
+        <input type="text" id="playerNameInput" ref={enteredName} />
+        <button onClick={() => setPlayerName(enteredName.current!.value)}>
+          Set Name
+        </button>
       </p>
     </section>
   );
