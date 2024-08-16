e083207cef59cdaad2749e6ec7c1280ea9d3f453
ibnabii <someone@somewhere.net>
2024-08-16 19:33:59 +0200

    no ref, but with two states

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState<string>("unknown entity");
+  const [enteredName, setEnteredName] = useState<string>("");
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          id="playerNameInput"
+          onChange={(e) => setEnteredName(e.target.value)}
+        />
+        <button onClick={() => setPlayerName(enteredName)}>Set Name</button>
       </p>
     </section>
   );
