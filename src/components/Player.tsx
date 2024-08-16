bdf90b12dea5d434d79723c3f579a32f8bf3e9b3
ibnabii <someone@somewhere.net>
2024-08-16 19:52:06 +0200

    clear after submitting name (should not happen if that's something more than clearing input)

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,15 +1,20 @@
 import { useRef, useState } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState<string>("unknown entity");
+  const [playerName, setPlayerName] = useState<string>("");
   const enteredName = useRef<HTMLInputElement>(null);
 
   return (
     <section id="player">
-      <h2>Welcome {playerName}</h2>
+      <h2>Welcome {playerName !== "" ? playerName : "unknown entity"}</h2>
       <p>
         <input type="text" id="playerNameInput" ref={enteredName} />
-        <button onClick={() => setPlayerName(enteredName.current!.value)}>
+        <button
+          onClick={() => {
+            setPlayerName(enteredName.current!.value);
+            enteredName.current!.value = "";
+          }}
+        >
           Set Name
         </button>
       </p>
