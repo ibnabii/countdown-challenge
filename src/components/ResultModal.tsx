e0ca2d40aa44955a1e4bbc235ba8708bff89ab20
ibnabii <someone@somewhere.net>
2024-08-17 09:54:13 +0200

    modal closes by esc

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -14,8 +14,9 @@ const ResultModal = forwardRef<resultModalHandle, resultModalProps>(
   function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     const dialog = useRef<HTMLDialogElement>(null);
 
-    const result = remainingTime <= 0 ? "lost" : "won";
+    const userLost = remainingTime <= 0;
     const formattedTimeRemaining = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
       return {
@@ -26,8 +27,9 @@ const ResultModal = forwardRef<resultModalHandle, resultModalProps>(
     });
 
     return (
-      <dialog className="result-modal" ref={dialog}>
-        <h2>You {result}!</h2>
+      <dialog className="result-modal" ref={dialog} onClose={onReset}>
+        {userLost && <h2>You lost!</h2>}
+        {!userLost && <h2>Your score: {score}</h2>}
         <p>
           The target time was <strong>{targetTime}</strong> seconds.
         </p>
@@ -36,7 +38,7 @@ const ResultModal = forwardRef<resultModalHandle, resultModalProps>(
           <strong>{formattedTimeRemaining} seconds left.</strong>
         </p>
         <form method="dialog">
-          <button onClick={onReset}>Close</button>
+          <button>Close</button>
         </form>
       </dialog>
     );
